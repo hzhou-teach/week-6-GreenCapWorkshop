3940bb762e3d77fa53e3571cc7f9ce2520988782
Brian Lu <lu_haoqin@outlook.com>
2020-07-21 17:47:04 -0500

    planting_silver

--- planting_silver.cpp
+++ planting_silver.cpp
@@ -1,7 +1,12 @@
 // Brian Lu
 // 15 mins in *xxxxxxx** what?
 // 35 mins in *xxxxxxx** what?
-// 
+// 40 mins in ********** turns out I just cannot read. 
+// Input was N-1 lines and I was some other garbage from an extra line or something
+//
+// For the design of this i just started drawing a lot of trees on paper and this strategy looked ok.
+// Pulled my hair out thinking it was wrong for a little while though.
+// I think the moral of the story is to read more when in doubt.
 
 #include <fstream>
 #include <iostream>
@@ -16,24 +21,21 @@ int main()
     int N;
     FILE *input = fopen("planting.in", "r");
     fscanf(input, "%d", &N);
-    int fields[N];
-    for (int i = 0; i < N; i++) {
-        fields[i] = 0;
-    }
-    for (int i = 0; i < N; i++) {
+    std::vector<int> fields[N];
+    for (int i = 0; i < N-1; i++) {
         int f1, f2;
-        fscanf(input, "%d", &f1);
-        fields[f1-1]++;
-        fscanf(input, "%d", &f2);
-        fields[f2-1]++;
+        fscanf(input, "%d %d", &f1, &f2);
+        fields[f1-1].push_back(f2-1);
+        fields[f2-1].push_back(f1-1);
     }
     fclose(input);
+
     int max = 0;
     for (int i = 0; i < N; i++) 
-        max = std::max(max, fields[i]);
+        max = std::max((size_t) max, fields[i].size());
 
     FILE *output = fopen("planting.out", "w");
-    fprintf(output, "%d\n", max);
+    fprintf(output, "%d\n", max+1);
     fclose(output);
 
     return 0;
